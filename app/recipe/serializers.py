71f89428f7bdb7238cb3a29927773b9246035625
uz2ee <uzzal.h.mohammad@gmail.com>
2022-09-25 01:00:46 +0600

    Tags Serializer added

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -4,7 +4,10 @@ Serializer for recipe api
 
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -30,3 +33,15 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """
+    Serialzier for tags
+    """
+    class Meta:
+        model = Tag
+        fields = [
+            'uuid',
+            'name',
+        ]
