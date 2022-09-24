799369444323b8b6dfa2490fa74effdd77c40fb9
uz2ee <uzzal.h.mohammad@gmail.com>
2022-09-25 01:00:31 +0600

    Added default permission

--- app/app/settings.py
+++ app/app/settings.py
@@ -157,7 +157,11 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
+
 }
 
 SIMPLE_JWT = {
