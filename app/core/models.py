85ce8dd24db45f06e541c66b9107165464a5b032
uz2ee <uzzal.h.mohammad@gmail.com>
2022-09-26 02:36:21 +0600

    Ingredients model adde with tests

--- app/core/models.py
+++ app/core/models.py
@@ -95,6 +95,7 @@ class Recipe(BaseModel):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
@@ -103,6 +104,16 @@ class Recipe(BaseModel):
 class Tag(BaseModel):
     """
     Tag for filtering recipe
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(BaseModel):
+    """
+    Ingredient for recipe
     """
     name = models.CharField(max_length=255)
 
