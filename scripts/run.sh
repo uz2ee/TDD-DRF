6cdc638d2c2bb0df4c80d61fade392d13eddb719
uz2ee <uzzal.h.mohammad@gmail.com>
2022-10-03 13:37:42 +0600

    Initial deployment update

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -e
+
+python maange.py wait_for_db
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+uwsgi --soket :9000 --workers 4 --master --enable-threads --module app.wsgi
