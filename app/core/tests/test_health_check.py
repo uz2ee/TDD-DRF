64739a4b5c0f95ed48ede4ea5b04dcd3f45dd878
uz2ee <uzzal.h.mohammad@gmail.com>
2022-10-22 01:19:44 +0600

    Test for health check added

---
+++ app/core/tests/test_health_check.py
@@ -0,0 +1,25 @@
+"""
+Test for the health check api
+"""
+
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class HealthCheckTests(TestCase):
+    """
+    Test health check api
+    """
+
+    def test_health_check(self):
+        """
+        Test health check api
+        """
+        client = APIClient()
+        url = reverse('health-check')
+        res = client.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
